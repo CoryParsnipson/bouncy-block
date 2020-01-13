36bccf25a93e52ddf2b0fbf7cca02ff0676047d7
Cory Parsnipson <cparsnipson@gmail.com>
2020-01-12 23:22:56 -0800

    Add score and level. Fine tune barrier difficulty.

--- Main.gd
+++ Main.gd
@@ -1,19 +1,28 @@
 extends Node
 
+export var score = 0
+export var level = 0
+export var to_next_level = 2
 export var barrier_speed = 7
+export var barrier_offset_min = 0
+export var barrier_offset_max = 0
+export var barrier_gap_radius = 120
+
+export var barrier_speed_max = 25
+export var barrier_wait_time_min = 1.5
+export var barrier_gap_radius_min = 70
 
 var bounds
 var game_started = false
-var gap_center_min
-var gap_center_max
+var previous_barrier_pos_x
 
 var active_barriers = Array()
 
 func _ready():
 	bounds = get_viewport().get_visible_rect()
 	
-	gap_center_min = int(bounds.position.y) + 100
-	gap_center_max = int(bounds.end.y) - 100
+	barrier_offset_min = int(bounds.position.y) + barrier_gap_radius
+	barrier_offset_max = int(bounds.end.y) - barrier_gap_radius
 	
 	# TODO: move barriers beyond viewport edge
 	reset()
@@ -26,10 +35,20 @@ func _process(delta):
 func _physics_process(delta):
 	if !game_started:
 		return
+		
+	previous_barrier_pos_x = $BarrierUpper.position.x
 	
 	# TODO: how to programmtically calculate when this node falls off the screen?
-	if $Barrier.position.x > bounds.position.x - 14:
-		$Barrier.position.x -= barrier_speed
+	if $BarrierUpper.position.x > bounds.position.x - 14:
+		$BarrierUpper.position.x -= barrier_speed
+	
+	if $BarrierLower.position.x > bounds.position.x - 14:
+		$BarrierLower.position.x -= barrier_speed
+		
+	if previous_barrier_pos_x >= $Player.position.x \
+		and $BarrierUpper.position.x <= $Player.position.x:
+		score += 1
+		$LevelCounter.text = str(score)
 
 func reset():
 	randomize()
@@ -45,16 +64,28 @@ func start_game():
 	$Player.disable_physics = false
 	$BarrierTimer.start()
 	
-func send_barrier(barrier = $Barrier):
-	barrier.position.y = randi() % (gap_center_max - gap_center_min) + gap_center_min
-	barrier.position.x = bounds.end.x + 14
+func send_barrier():
+	$BarrierUpper.position.x = bounds.end.x + 14
+	$BarrierLower.position.x = bounds.end.x + 14
+	
+	var barrier_offset = randi() % (barrier_offset_max - barrier_offset_min) + barrier_offset_min
+	
+	$BarrierUpper.position.y = barrier_offset - barrier_gap_radius
+	$BarrierLower.position.y = barrier_offset + barrier_gap_radius
 	
 	#active_barriers.push(barrier)
 
 func _on_BarrierTimer_timeout():
 	send_barrier()
-	
-	$BarrierTimer.wait_time -= 0.02
-	barrier_speed += 1.02
+
+	if score % to_next_level == 0:
+		level += 1
+		
+		$BarrierTimer.wait_time = max($BarrierTimer.wait_time - 0.02, barrier_wait_time_min)
+		barrier_speed = min(barrier_speed + 1, barrier_speed_max)
+		barrier_gap_radius = max(barrier_gap_radius - 5, barrier_gap_radius_min)
+		
+		barrier_offset_min = int(bounds.position.y) + barrier_gap_radius
+		barrier_offset_max = int(bounds.end.y) - barrier_gap_radius
 	
 	$BarrierTimer.start()
